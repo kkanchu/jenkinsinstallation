cf45e955d5b2c339193cbe73a67543b496d09a08
root <root@jenkinsvm1.155ptpvv2j2ubjmc1hpldcflff.bx.internal.cloudapp.net>
2021-07-04 12:38:19 +0000

    added -y

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 sudo apt update
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 echo 'deb https://pkg.jenkins.io/debian binary/' >> /etc/apt/sources.list
-sudo apt install openjdk-8-source
+sudo apt install openjdk-8-source -y
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
